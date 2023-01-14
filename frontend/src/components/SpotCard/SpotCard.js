47c57762ce2b07f9fe2da67d160eecb10f878ab5
heidisong0814 <heidisong0814@gmail.com>
2023-01-14 09:25:04 -0800

    image-formatting

--- frontend/src/components/SpotCard/SpotCard.js
+++ frontend/src/components/SpotCard/SpotCard.js
@@ -14,6 +14,7 @@ const SpotCard = ({spot}) => {
     console.log(spot)
     return (
         <div className="spot-card">
+            <div><img className="spot-card-preview-image" src="https://images.unsplash.com/photo-1501183638710-841dd1904471?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"></img></div>
             <Link to={`/spots/${spot.id}`} onClick ={getDetailsHandler}>
             <div className="spot-name">{spot.name}</div>
             </Link>
